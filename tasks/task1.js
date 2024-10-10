7bfc4f2f6e95cec8a2be388bbfe177d52aed2141
Yana Maksymenko <jane.vgame@gmail.com>
2024-10-10 11:05:05 +0300

    lab5_Maksymenko

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,7 +1,14 @@
 //Функція filterAdults приймає масив об'єктів, що представлє людей (з полями name та age),
 //вам потрібно написати код, який повертає новий масив, який містить лише тих людей, чий вік більше або дорівнює 18.
-function filterAdults(people) {
-  //Ваш код
+function filterAdults(people)
+ {
+  let adults = []; // Ініціалізуємо масив для дорослих
+
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].age >= 18) adults.push(people[i]); // Додаємо дорослих без додаткового блоку
+  }
+
+  return adults; // Повертаємо масив дорослих
 }
 
 module.exports = filterAdults;
--- tasks/task3.js
+++ tasks/task3.js
@@ -1,6 +1,11 @@
-// З масива людей(people) Вам потрібно знайти людину за іменем(name)
-function findByName(people, name) {
-  // Ваш код
+function findByName(people, name) 
+{
+  for (let i = 0; i < people.length; i++)
+     {
+    if (people[i].name === name) 
+      {
+      return people[i]; 
+    }
+  }
 }
-
 module.exports = findByName;
